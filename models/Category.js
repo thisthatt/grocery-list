cd788664f3976ffd184ab38c1c4442ea91fdbc48
robertsonj48 <robertsonj48@gmail.com>
2022-08-31 19:29:57 -0400

    first commit

---
+++ models/Category.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const CategorySchema = new mongoose.Schema({
+  category: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Category', CategorySchema)
---
+++ models/Todo.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const TodoSchema = new mongoose.Schema({
+  todo: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+  }
+})
+
+module.exports = mongoose.model('Todo', TodoSchema)
