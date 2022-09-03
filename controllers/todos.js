dec759bb4434b234498d69821dddae22a02a5852
Tron Nguyen <pnguye894@gmail.com>
2022-09-03 16:39:44 -0400

    Add title to view pages

--- controllers/todos.js
+++ controllers/todos.js
@@ -7,7 +7,7 @@ module.exports = {
         try{
             const todoItems = await Todo.find({userId:req.user.id})
             const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
-            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user})
+            res.render('todos.ejs', {title:'Homepage',todos: todoItems, left: itemsLeft, user: req.user})
         }catch(err){
             console.log(err)
         }
