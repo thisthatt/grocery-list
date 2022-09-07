b59273f73e8b07531b0dc523b8fff388fdb52f2d
Yazcodes <88701749+thisthatt@users.noreply.github.com>
2022-09-06 20:05:33 -0400

    added todo and category checks, todo timestamp

--- models/Category.js
+++ models/Category.js
@@ -4,16 +4,17 @@ const CategorySchema = new mongoose.Schema({
   category: {
     type: String,
     required: true,
+    maxLength: [50, "Category name should be less than 50 characters"],
   },
-  userId:{
-    type:String,
-    required:true
+  userId: {
+    type: String,
+    required: true,
   },
-  display:{
-    type:Boolean,
-    default:false,
-    required:true
-  }
-})
+  display: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+});
 
 module.exports = mongoose.model('Category', CategorySchema)
